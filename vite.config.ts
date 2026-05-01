079fa856b1cb23e6a6227a6a054c278b7ba5c621
MTSK <magnatsk3@hotmail.com>
2026-05-01 18:02:42 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/send-it-zip-magic/", // Línea añadida para GitHub Pages
   server: {
     host: "::",
     port: 8080,
